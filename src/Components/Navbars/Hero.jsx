4693258a8c363207903d52be3407eac500712c8d
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-21 17:24:03 +0600

    anaimation text added and animation svg added to my ui section

--- src/Components/Navbars/Hero.jsx
+++ src/Components/Navbars/Hero.jsx
@@ -2,11 +2,14 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="hero  bg-base-200">
+    <div className="hero  -mt-16">
       <div className="hero-content text-center">
         <div className="">
           <h1 className="text-5xl font-bold">
-            Welcome to <span className="text-primary">ByteBlaze</span>
+            Welcome to{" "}
+            <span className="bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient">
+              ByteBlaze
+            </span>
           </h1>
           <p className="py-6">
             ByteBlaze is the bridge between the complex world of technology and{" "}
@@ -16,13 +19,13 @@ const Hero = () => {
           <div className="space-x-4">
             <Link
               to="/blogs"
-              className="btn border-2 shadow-purple-800 border-purple-700 font-bold"
+              className="btn hover:bg-primary border-2 shadow-purple-800 border-purple-700 font-bold"
             >
               Read Blogs
             </Link>
             <Link
               to="/bookmarks"
-              className="border-2 font-bold border-purple-600 shadow-purple-800  btn"
+              className="border-2 font-bold hover:bg-purple-800 border-purple-400 shadow-purple-800  btn"
             >
               Bookmarks
             </Link>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import Hero from "../Components/Navbars/Hero";
-
+import wave from "../assets/img/wave.svg";
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-120px)]">
+    <div className="flex relative flex-col items-center justify-center min-h-[calc(100vh-120px)]">
       <Hero></Hero>
+      <img className="absolute bottom-0 w-full" src={wave} alt="" />
     </div>
   );
 };
