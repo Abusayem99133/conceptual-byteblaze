6fe4966ab7da5bd3a71b2d9f5784249f04e73e1f
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-24 23:18:07 +0600

    localstorage stored data delet

--- src/Components/Navbars/BlogCard/BlogCard.jsx
+++ src/Components/Navbars/BlogCard/BlogCard.jsx
@@ -1,22 +1,40 @@
+import { MdDeleteForever } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { deleteBlog } from "../../../Utils";
 
-const BlogCard = ({ blog }) => {
+const BlogCard = ({ blog, deletable, handleBookmark }) => {
   const { cover_image, title, description, published_at, id } = blog;
+
   return (
-    <Link to={`/blog/${id}`}>
-      <a className="max-w-sm mx-auto group  hover:no-underline focus:no-underline bg-gray-900">
-        <img
-          role="presentation"
-          className="object-cover w-full rounded h-44 bg-gray-500"
-          src={cover_image}
-        />
-        <div className="p-6 space-y-2">
-          <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline"></h3>
-          <span className="text-xs text-gray-400">{published_at}</span>
-          <p></p>
+    <div className="flex relative mt-12">
+      <Link to={`/blog/${id}`}>
+        <a className="max-w-sm mx-auto group  hover:no-underline focus:no-underline bg-gray-900">
+          <img
+            role="presentation"
+            className="object-cover w-full rounded h-44 bg-gray-500"
+            src={cover_image}
+          />
+          <div className="p-6 space-y-2">
+            <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+              {title}
+            </h3>
+            <span className="text-xs text-gray-400">{published_at}</span>
+            <p>{description}</p>
+          </div>
+        </a>
+      </Link>
+      {deletable && (
+        <div
+          onClick={() => handleBookmark()}
+          className="absolute bg-primary p-3 rounded-full hover:scale-105 hover:bg-purple-400 -top-5 -right-5"
+        >
+          <MdDeleteForever
+            size={20}
+            className="text-secondary group-hover:text-primary"
+          />
         </div>
-      </a>
-    </Link>
+      )}
+    </div>
   );
 };
 
--- src/Pages/Bookmarks.jsx
+++ src/Pages/Bookmarks.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { getBlogs } from "../Utils";
+import BlogCard from "../Components/Navbars/BlogCard/BlogCard";
+
 const Bookmarks = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const storedBlogs = getBlogs();
+    setBlogs(storedBlogs);
+    const handleBookmark = (id) => {
+      deleteBlog(id);
+      const storedBlogs = getBlogs();
+      setBlogs(storedBlogs);
+    };
+  }, []);
   return (
-    <div>
-      <h1>Bookmarks page</h1>
+    <div className="grid px-4 sm:px-8 lg:px-12 py-4 justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {blogs.map((blog) => (
+        <BlogCard
+          handleBookmark={handleBookmark}
+          deletable={true}
+          key={blog.id}
+          blog={blog}
+        ></BlogCard>
+      ))}
     </div>
   );
 };
