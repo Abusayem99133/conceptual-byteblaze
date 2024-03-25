c52ebf51efb291c24c508664ed6a559f2d503e71
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-25 11:56:54 +0600

    full section functonal work done maybe

--- src/Pages/Bookmarks.jsx
+++ src/Pages/Bookmarks.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { deleteBlog, getBlogs } from "../Utils";
 import BlogCard from "../Components/Navbars/BlogCard/BlogCard";
+import EmptyState from "./EmptyState";
 
 const Bookmarks = () => {
   const [blogs, setBlogs] = useState([]);
@@ -13,6 +14,14 @@ const Bookmarks = () => {
     const storedBlogs = getBlogs();
     setBlogs(storedBlogs);
   };
+  if (blogs.length < 1)
+    return (
+      <EmptyState
+        message="No Bookmark Available !"
+        address={"/blogs"}
+        label={"Go To Blogs"}
+      ></EmptyState>
+    );
   return (
     <div className="grid px-4 sm:px-8 lg:px-12 py-4 justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {blogs.map((blog) => (
